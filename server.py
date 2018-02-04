b12e74d8081300ac24f93aa7aa87c542366059c5
Unknown <andrsolo21@gmail.com>
2018-02-04 21:51:43 +0300

    не работает

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QWidget,QDesktopWidget, QPushButton,QHBoxLayout, QFrame,QSplitter, QStyleFactory, QApplication
+from PyQt5.QtWidgets import QWidget,QDesktopWidget , QLabel, QGridLayout, QLineEdit, QHBoxLayout, QVBoxLayout,QHBoxLayout, QFrame,QSplitter, QStyleFactory, QApplication, QMainWindow, QPushButton, QApplication
 from PyQt5.QtGui import QPainter, QColor, QPen, QIcon, QBrush
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import pyqtSignal, QObject,Qt,QCoreApplication
 import json
 import random
 import socket
@@ -384,8 +384,11 @@ class GameServer:
 
 
 
-
-class painter(QWidget):
+class Communicate(QObject):
+    st = 'first'
+    n = 0
+    closeApp = pyqtSignal()
+class MAIN_painter(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -459,11 +462,135 @@ class painter(QWidget):
                 background.setPen(QColor(255, 255, 255))
                 background.setFont(QFont('Decorative', 10))
                 background.drawText(self.karta[i][j][0][0],self.karta[i][j][1][0], self.yi,self.yi,Qt.AlignCenter, Point.info[GameConfig.map[i][j]]['multi'])"""
-        
-        
+class FIRST_painter(QMainWindow):
+
+    def __init__(self):
+        """первое окно\nдве кнопки: подключиться и создать игру"""
+        super().__init__()
+        self.st = ''
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        btncreate = QPushButton("Create game", self)
+        btncreate.move(30, 50)
+
+        btnconnect = QPushButton("Connect game", self)
+        btnconnect.move(150, 50)
+
+        btncreate.clicked.connect(self.buttoncreate)
+        btnconnect.clicked.connect(self.buttonconnect)
+
+        self.statusBar()
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Event sender')
+        self.show()
+
+
+    def buttoncreate(self):
+
+        sender = self.sender()
+        self.statusBar().showMessage(sender.text() + ' was pressed')
+
+
+    def buttonconnect(self):
+
+        sender = self.sender()
+        self.statusBar().showMessage(sender.text() + ' was pressed')
+        iwantpaint.createnewform(2)
+class CONNECT_painter(QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+        self.st = ''
+
+        self.initUI()
+
+
+    def initUI(self):
+
+
+
+        self.koef = 0.8
+        self.k2 = 0.3
+        self.widthtotal = self.koef * QDesktopWidget().availableGeometry().width()*0.5
+        self.heighttotal = self.koef * QDesktopWidget().availableGeometry().height() - 30
+        self.ot = QDesktopWidget().availableGeometry().width() / 100
+        self.libw = self.widthtotal * self.k2
+        self.libh = self.heighttotal / 3
+        self.yi = self.widthtotal * (1 - 2* self.k2) / 15 - self.ot
+        """инициализация окна"""
+        self.setGeometry(0, 30, self.widthtotal, self.heighttotal)
+        self.setWindowTitle('Event sender')
+        self.show()
+
+        btnconcreate = QPushButton("Подключиться", self)
+        btnfirst = QPushButton("Вернуться назад", self)
+
+        title = QLabel('Title')
+        title.setAlignment(Qt.AlignCenter)
+        title.setStyleSheet("QLabel {background-color: red;}")
+        status = QLineEdit('')
+        title2 = QLabel('Title2')
+        title2.setAlignment(Qt.AlignCenter)
+        title2.setStyleSheet("QLabel {background-color: yellow;}")
+
+        grid = QGridLayout()
+        grid.setSpacing(8)
+
+        btnconcreate.clicked.connect(self.buttonconcreate)
+        btnfirst.clicked.connect(self.buttonfirst)
+
+        grid.addWidget(title, 0, 0,2,3)
+        grid.addWidget(title2, 4, 0,1,3)
+        grid.addWidget(status,2,0,1,3)
+
+
+        grid.addWidget(btnfirst, 1, 4,1,1)
+        grid.addWidget(btnconcreate, 0, 4,1,1)
+
+
+        self.setLayout(grid)
+
+        #btnfirst.clicked.connect(QCoreApplication.instance().quit)
+        '''vbox = QVBoxLayout()
+        vbox.addStretch(3)
+        vbox.addWidget(btnconcreate)
+        vbox.addWidget(btnfirst)
+        hbox = QHBoxLayout()
+        hbox.addStretch(3)
+        hbox.addLayout(vbox)
+        self.setLayout(hbox)'''
+
+        #self.statusBar()
+
+    def buttonconcreate(self):
+
+        pass
+
+    def buttonfirst(self):
+        iwantpaint.createnewform(1)
+
+class iwantpaint():
+
+    def __init__(self):
+        pass
+    def createnewform(number):
+
+        app = QApplication(sys.argv)
+        if number == 1:
+            ex1 = FIRST_painter()
+        elif number == 2:
+            ex2 = CONNECT_painter()
+        elif number == 3:
+            ex3 = MAIN_painter()
+        sys.exit(app.exec_())
 
 if __name__ == '__main__':
+    i = iwantpaint
+    i.createnewform(1)
 
-    app = QApplication(sys.argv)
-    ex = painter()
-    sys.exit(app.exec_())
