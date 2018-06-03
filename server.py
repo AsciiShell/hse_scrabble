ec2cdec72551c02cc8d08cebfc860ba753491050
Unknown <asciishell@gmail.com>
2018-06-03 15:24:22 +0300

    Удаление бонусов после хода

--- server.py
+++ server.py
@@ -319,6 +319,8 @@ class GameServer:
                 self._give_letter(self.players[player])
                 result = self.players[player].action()
                 self.matrix.acceptedWords.extend(result.words)
+                for i in result.letters:
+                    GameConfig.map[i.x][i.y] = 0
                 if result.changed:
                     skip = 0
                 else:
